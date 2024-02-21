07bea5cd567e09c6dee1afd4ee2128e988aff84c
ZzinzZ <nhata338@gmail.com>
2024-02-21 15:10:20 +0700

    MVC structure

---
+++ src/modules/index/index.controller.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request,Response } from "express";
+
+
+export default class IndexController {
+    public index = (req:Request, res: Response, next: NextFunction) => {
+        try {
+            res.status(200).send('Api is available')
+        } 
+        catch (err) {
+            next(err);
+        }
+    }
+}
